de47be84fa1c4f6ba52bdd11beae5f6232b28d47
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2015-02-22 20:48:24 +0100

    Solution to the blackjack

--- app/controllers/blackjack_controller.rb
+++ app/controllers/blackjack_controller.rb
@@ -3,29 +3,47 @@ class BlackjackController < ApplicationController
 	include BlackjackHelper
 
 	def game
-		start_cards = pick_card(CARDS)
-		@player_cards = params[:player_cards] || [] << start_cards
-		@player_points = params[:player_points] || value(start_cards, 0)
-		@type = params[:type] || "Player"
-		@card_name = @player_cards.first.keys.last
-		
-		@bank_cards ||= []
-		@bank_points ||= 0
-		@test = params.fetch(:hit) if params[:hit] #if is true  #@test = params["hit"]  {"hit"=>"Max", "controller"=>"blackjack", "action"=>"game"}
+		player_start_card = pick_card(CARDS)
+		@player_cards = params[:player_cards] || [] << player_start_card
+		@player_points = params[:player_points] || value(player_start_card, 0)
+		@player_card_name = @player_cards.first.keys.last
+
+		@stand = params[:stand] || false
+
+		bank_start_card = pick_card(CARDS)
+		@bank_cards = params[:bank_cards] || [] << bank_start_card
+		@bank_points = params[:bank_points] || value(bank_start_card, 0)
+		@bank_card_name = @bank_cards.first.keys.last
 	end
 
 	def hit
-		pp = params[:test].to_i
+		pp = params[:pp].to_i
 		pc = params[:pc]
-
 		@player_cards = params.fetch(:pc)
 		@player_points = calculation(pp, pc)
-		redirect_to root_path(:player_points => @player_points, :player_cards => @player_cards, :type => "Player")
+
+		bp = params[:bp].to_i
+		bc = params[:bc]
+		@bank_cards = params.fetch(:bc)
+		@bank_points = params.fetch(:bp)
+
+		redirect_to root_path(:player_points => @player_points, :player_cards => @player_cards, :bank_points => @bank_points, :bank_cards => @bank_cards)
 	end
 
 	def stand
-		@stand_resolution = "You stand with #{player_points} points"	
-		redirect_to root_path(:stand => @stand_resolution)
+		@bank_cards = params[:bc]
+		@bank_points = params[:bp].to_i
+
+		@player_cards = params.fetch(:pc)
+		@player_points = params.fetch(:pp)
+
+		@stand = true
+
+		while @bank_points < 17
+			@bank_points = calculation(@bank_points, @bank_cards)
+		end
+
+		redirect_to root_path(:stand => @stand, :player_points => @player_points, :player_cards => @player_cards, :bank_points => @bank_points, :bank_cards => @bank_cards)
 	end
 
 
--- app/helpers/blackjack_helper.rb
+++ app/helpers/blackjack_helper.rb
@@ -14,96 +14,55 @@ module BlackjackHelper
 		{"King_Clubs"=>10}, {"King_Hearts"=>10}, {"King_Spades"=>10}, {"King_Diamonds"=>10},
 		{"Ace_Clubs"=>[1,11]}, {"Ace_Hearts"=>[1,11]}, {"Ace_Spades"=>[1,11]}, {"Ace_Diamonds"=>[1,11]}]
 
-
-	def introduction_game
-		puts "Welcome to the Blackjack game!"
-		puts "Rules:\nThe Bank stand at 17.\nInsurance pays 2 to 1" 
-	end
-
-	def hit_or_stand
-		puts "\nDo you want a card?? (hit or stand)"
-		gets.chomp
-	end
-
 	def pick_card(cards)
 		cards.delete_at(rand(0...cards.size))
 	end
 
-	def value(card, player_points) 
+	def value(card, player_points)
 		if card.values.first.class == String
-				card.values.first.to_i 
+				card.values.first.to_i
 		elsif card.values.first.class == Array && player_points <= 11
 				card.values.first.last.to_i
 		else
-				 card.values.first.to_i	   
+				 card.values.first.to_i
 		end
 	end
 
-	def resolution(player_points, bank_points)
-		puts
-		puts "Resolution:"
-		puts "Your points: #{player_points} vs Bank points: #{bank_points}"
+	def card_info(type, card_name, total_points)
+		"<h4>#{type}'s card show #{card_name}. #{type} now has #{total_points} total points.</h4>".html_safe
+	end
 
-		if player_points <= 21 && bank_points <= 21
-			if player_points > bank_points 
-				puts "You won! :) Congratulation !!" 
-			elsif player_points < bank_points
-				puts "You lose! :( maybe next time mate..." 
+	def resolution(player_points, bank_points)
+		if player_points.to_i <= 21 && bank_points.to_i <= 21
+			if player_points.to_i > bank_points.to_i
+				"<h1>You won! :) Congratulation !!</h1>".html_safe
+			elsif player_points.to_i < bank_points.to_i
+				"<h1>You lose! :( maybe next time mate...</h1>".html_safe
 			else
-				puts "It's a tie!"
+				"<h1>It's a tie!</h1>".html_safe
 			end
-		elsif player_points > 21 && bank_points <= 21
-				puts "You lose! :( maybe next time mate..." 	
-		elsif player_points <= 21 && bank_points > 21
-				puts "You won! :) Congratulation !!" 
+		elsif player_points.to_i > 21 && bank_points.to_i <= 21
+				"<h1>You lose! :( maybe next time mate...</h1>".html_safe
+		elsif player_points.to_i <= 21 && bank_points.to_i > 21
+				"<h1>You won! :) Congratulation !!</h1>".html_safe
 		else
-				puts "Both lose :/"
+				"<h1>Both lose :/</h1>".html_safe
 		end
 	end
 
 	def player_feedback(player_points)
-		if player_points > 21
-		puts "You went too far :(\n"
-		elsif player_points == 21 
-		puts "Oh! Blackjack!! ;)\n"
+		if player_points.to_i > 21
+			"<h1>You went too far :(</h1>".html_safe
+		elsif player_points.to_i == 21
+			"<h1>Oh! Blackjack!! ;)</h1>".html_safe
 		end
 	end
 
 	def calculation(points, player_cards)
 		playercard = pick_card(CARDS)
 		player_cards << playercard
-		#card_name = playercard.keys.first
 		card_value = value(playercard, points)
 		points += card_value
-		#puts "\n#{type} got #{card_name}(#{card_value} points)..\n#{type} have #{points} points"
-		points
 	end
-
-	# introduction_game
-
-	# player_cards = []
-	# player_points = 0
-	# bank_cards = []
-	# bank_points = 0
-
-	# while player_points < 21
-	# 	decision = hit_or_stand
-	# 	if decision != "hit" 
-	# 		puts "You stand with #{player_points} points"	
-	# 		break
-	# 	else
-	# 		player_points = calculation(player_points, player_cards, "Player")
-	# 	end
-	# end
-
-	# player_feedback(player_points)
-
-
-	# while bank_points < 17
-	# 	bank_points = calculation(bank_points, bank_cards, "Bank")
-	# end
-
-
-	# resolution(player_points, bank_points)
 
 end
